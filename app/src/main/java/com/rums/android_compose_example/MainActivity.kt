6bab17bd4bffaf9d20e0cd072f0bd6b5db6992dc
“RumitPatel” <rumit.patel5@gmail.com>
2022-11-29 09:07:13 +0530

    small update.

--- app/src/main/java/com/rums/android_compose_example/MainActivity.kt
+++ app/src/main/java/com/rums/android_compose_example/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rums.android_compose_example.ui.theme.AndroidcomposeexampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    SearchBar()
                 }
             }
         }
@@ -39,7 +44,8 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     AndroidcomposeexampleTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        SearchBar(Modifier.padding(8.dp))
     }
 }
 
@@ -47,5 +53,20 @@ fun DefaultPreview() {
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
-    TextField(value = "Hello", onValueChange = {})
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(Icons.Default.Search, contentDescription = null)
+        },
+        placeholder = {
+            Text(stringResource(id = R.string.app_name))
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        modifier = modifier
+            .heightIn(min = 56.dp)
+            .fillMaxWidth()
+    )
 }
