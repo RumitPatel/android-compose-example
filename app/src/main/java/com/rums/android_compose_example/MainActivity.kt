8534cb27230414fbb588e2df2b4a8efa342c6393
“RumitPatel” <rumit.patel5@gmail.com>
2022-11-30 08:47:30 +0530

    Colum item created and passed dynamic value from resource.

--- app/src/main/java/com/rums/android_compose_example/MainActivity.kt
+++ app/src/main/java/com/rums/android_compose_example/MainActivity.kt
@@ -3,17 +3,24 @@ package com.rums.android_compose_example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rums.android_compose_example.ui.theme.AndroidcomposeexampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +51,12 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     AndroidcomposeexampleTheme {
 //        Greeting("Android")
-        SearchBar(Modifier.padding(8.dp))
+//        SearchBar(Modifier.padding(8.dp))
+        ListElement(
+            modifier = Modifier.padding(8.dp),
+            drawable = R.drawable.demo_image,
+            stringID = R.string.my_demo_text
+        )
     }
 }
 
@@ -68,4 +80,32 @@ fun SearchBar(
             .heightIn(min = 56.dp)
             .fillMaxWidth()
     )
+}
+
+@Composable
+fun ListElement(
+    modifier: Modifier = Modifier,
+    @DrawableRes drawable: Int,
+    @StringRes stringID: Int
+) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(id = drawable),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            stringResource(id = stringID),
+            fontSize = 15.sp,
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+
+        )
+    }
 }
