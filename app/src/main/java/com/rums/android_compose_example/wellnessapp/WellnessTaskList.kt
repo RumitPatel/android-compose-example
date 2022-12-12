024ef819208fb1569c474a45de5cc7c19c4639da
RumitEmed <rumit.emed@gmail.com>
2022-12-12 21:49:13 +0530

    Working on WellnessTaskList.

--- app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessTaskList.kt
+++ app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessTaskList.kt
@@ -1,12 +1,23 @@
 package com.rums.android_compose_example.wellnessapp
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
 
 @Composable
-fun WellnessTaskList (
-/*    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks}*/
-){
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+
+        }
+    }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
