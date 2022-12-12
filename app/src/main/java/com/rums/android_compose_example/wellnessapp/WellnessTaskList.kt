04eaa05749d6e3aeeb1df642770f5672b4d49458
RumitEmed <rumit.emed@gmail.com>
2022-12-12 21:43:26 +0530

    WellnessTaskList.kt created.

---
+++ app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessTaskList.kt
@@ -0,0 +1,12 @@
+package com.rums.android_compose_example.wellnessapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
+
+@Composable
+fun WellnessTaskList (
+/*    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = remember { getWellnessTasks}*/
+){
+}
