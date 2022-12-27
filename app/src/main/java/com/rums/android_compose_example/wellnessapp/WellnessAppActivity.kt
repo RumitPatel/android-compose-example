261a0f31f8712b4a4ba3259f29f576f9ad9adc83
RumitPatel <rumit.patel5@gmail.com>
2022-12-27 10:53:28 +0530

    Longpress implemented.

--- app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessAppActivity.kt
+++ app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessAppActivity.kt
@@ -1,19 +1,31 @@
 package com.rums.android_compose_example.wellnessapp
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
 import com.rums.android_compose_example.ui.theme.AndroidcomposeexampleTheme
 
 class WellnessAppActivity : ComponentActivity() {
+
+    private lateinit var mContext: Context
+    private val viewModel: WellnessViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mContext = this
+
+        initObserver()
+
         setContent {
             AndroidcomposeexampleTheme {
                 Surface(
@@ -23,6 +35,14 @@ class WellnessAppActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun initObserver() {
+        viewModel.message.observe(this, Observer {
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
 
