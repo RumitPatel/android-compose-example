62519d7f1880625b8ed2ce8a84426c8bcdc22ebe
RumitPatel <rumit.patel5@gmail.com>
2023-01-03 11:58:35 +0530

    removed temp Preview.

--- app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessScreen.kt
+++ app/src/main/java/com/rums/android_compose_example/wellnessapp/WellnessScreen.kt
@@ -6,11 +6,9 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-@Preview
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
