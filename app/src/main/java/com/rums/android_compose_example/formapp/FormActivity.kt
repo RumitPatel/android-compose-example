ce6bad136a026c7817e03aacbdeef3d9271e295b
RumitPatel <rumit.patel5@gmail.com>
2023-01-12 10:12:16 +0530

    FormViewModel.kt removed.

--- app/src/main/java/com/rums/android_compose_example/formapp/FormActivity.kt
+++ app/src/main/java/com/rums/android_compose_example/formapp/FormActivity.kt
@@ -3,10 +3,8 @@ package com.rums.android_compose_example.formapp
 import android.content.Context
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -19,13 +17,10 @@ import com.rums.android_compose_example.utils.toast
 class FormActivity : ComponentActivity() {
 
     private lateinit var mContext: Context
-    private val formViewModel: FormViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
-
-        initObserver()
 
         setContent {
             AndroidcomposeexampleTheme {
@@ -47,14 +42,6 @@ class FormActivity : ComponentActivity() {
                         }
                     })
                 }
-            }
-        }
-    }
-
-    private fun initObserver() {
-        formViewModel.message.observe(this) { it ->
-            it.getContentIfNotHandled()?.let {
-                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
             }
         }
     }
